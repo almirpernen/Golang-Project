5fb0aee9bdb5b470edf91e20d1d3f044cc951311
Arman <devilman@mail.com>
2024-05-04 11:27:36 +0500

    version 0.1.5

--- ECOM/go.mod
+++ ECOM/go.mod
@@ -8,4 +8,7 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
+	gorm.io/gorm v1.25.10 // indirect
 )
--- ECOM/service/user/routes.go
+++ ECOM/service/user/routes.go
@@ -1,17 +1,22 @@
 package user
 
 import (
-	
 	"net/http"
 
+	"github.com/almirpernen/types"
+	"github.com/almirpernen/utils"
 	"github.com/gorilla/mux"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/godoc/util"
+	"gorm.io/gorm/utils"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -29,6 +34,33 @@ func (h *Handler) handleRegister(w http.ResponseWriter,r *http.Request) {
     // do something
 
 	//get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload) ; err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 	//check if the user exists
+	_, err. := h.store.GetUserByEmail(payload.Email)
+	if err == nil
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email
+		&s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err:= auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 	// if it doesnt we create the new user
+	err = h.store.CreateUser(types.User{
+		FirstName: 	payload.FirstName,
+		LastName: 	payload.LastName,
+		Email: 		payload.Email,
+		Password: 	hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w,http.StatusInternalServerError, err)
+		return
+	}
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
